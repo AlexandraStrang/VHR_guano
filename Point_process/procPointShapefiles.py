08eb9f0b8b572fe107f043e159fc0a36e382aa39
Alexandra Strang <alexandra.strang@pg.canterbury.ac.nz>
2025-07-08 14:58:14 +1200

    Script for getting xy point data in EPSG 3031

--- Point_process/procPointShapefiles.py
+++ Point_process/procPointShapefiles.py
@@ -1,6 +1,5 @@
 # Python script for turning penguin points into XY data (EPSG:3031)
 # Copyright (C) 2025 Alexandra Strang and Dean Anderson
-# NOT DONE JUST TESTING
 
 import os
 import pandas as pd
@@ -9,20 +8,22 @@ from osgeo import ogr, osr
 
 class Params:
     def __init__(self):
-        # LOOK AT ROYDS FIRST
-
         # DATA FOLDER
-        self.DataPath = r"D:\Royds_points_test"
+        self.DataPath = r"D:\Points_test"
 
         # DEM FILE AND POINT SHAPEFILE
         # COULD HAVE IMPORTED FROM ALEXANDRA DATA
         self.RoydsDEMFName = os.path.join(self.DataPath, 'Cape_Royds_clipped.tif')
         self.RoydsPointsFName = os.path.join(self.DataPath, 'royd_masked_labels_cleaned_coords_added_2020-12-01.shp')
-
+        
+        self.CrozierDEMFName = os.path.join(self.DataPath, 'Cape_Crozier_clipped.tif')
+        self.CrozierPointsFName = os.path.join(self.DataPath, 'croz_masked_labels_cleaned_coords_added_2020-11-29.shp')
+ 
         # OUTPUT DATA FILENAME
         self.RoydsPointsCSV = os.path.join(self.DataPath, 'Royds_Points_3031.csv')
+        self.CrozierPointsCSV = os.path.join(self.DataPath, 'Crozier_Points_3031.csv')
 
-        self.colonies = ['Royds']
+        self.colonies = ['Royds', 'Crozier']
 
 
 class DataProcessor:
@@ -56,7 +57,7 @@ class DataProcessor:
 
             # TRANSFORM CRS TO EPSG:3031
             target_srs = osr.SpatialReference()
-            target_srs.ImportFromEPSG(3031)  # Your DEM is in EPSG:3031
+            target_srs.ImportFromEPSG(3031)  # DEM is in EPSG:3031
             coord_transform = osr.CoordinateTransformation(source_srs, target_srs)
 
             # GET X Y DATA OF PENGUIN LOCATIONS
