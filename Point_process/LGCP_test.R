f19a47d2a7ee5efa6a7f92968439f32adcad82ac
Alexandra Strang <alexandra.strang@pg.canterbury.ac.nz>
2025-07-25 10:16:29 +1200

    Update LGCP_test.R

--- Point_process/LGCP_test.R
+++ Point_process/LGCP_test.R
@@ -7,11 +7,12 @@ library(fmesher)
 library(ggplot2)
 library(INLA)
 library(ggpubr)
+library(inlabru) # for lgcp()
 
-# best mesh so far Crozier 2020
-# use Crozier 2019 as testing 
-# EPSG 3031 is in metres but geom_sf will convert to degrees
-# mesh previews = meshbuilder()
+# sessionInfo() - important to note r version and versions of inlabru, INLA, fmesher
+# R version 4.5.0 (2025-04-11 ucrt)
+
+# EPSG 3031 is in metres but when plotting geom_sf will convert to degrees
 
 # read in points from xy csv
 # Cape Crozier 2020
@@ -33,22 +34,22 @@ sf_Crozier_guano <- st_transform(sf_Crozier_guano, crs = st_crs(sf_Crozier))
 Crozier_max.edge <- diff(range(st_coordinates(sf_Crozier)[,1]))/(3*5)
 # 160 metres
 
-# expand inner layer same amount (1/5)
+# expand outer layer out by 1/5
 Crozier_bound.outer = diff(range(st_coordinates(sf_Crozier)[,1]))/5
 # 500 metres
 
-# create Crozier mesh
-Crozier_mesh5 <- fm_mesh_2d(boundary = sf_Crozier_guano,
+# create finer Crozier mesh
+Crozier_mesh <- fm_mesh_2d(boundary = sf_Crozier_guano,
                             max.edge = c(1,5)*Crozier_max.edge, # inner and outer max edge where outer layer has triangle density lower than inner
-                            offset = c(Crozier_max.edge, Crozier_bound.outer/2),
-                            cutoff = Crozier_max.edge/10, # reduced cut off?
+                            offset = c(Crozier_max.edge, Crozier_bound.outer),
+                            cutoff = Crozier_max.edge/10,
                             crs = st_crs(sf_Crozier))
 
 # plot GA boundary mesh with points
-mesh5.5_plot_Crozier <- ggplot() + 
-  geom_fm(data = Crozier_mesh5) + 
+mesh_plot_Crozier <- ggplot() + 
+  geom_fm(data = Crozier_mesh) + 
   geom_sf(data = sf_Crozier_guano, fill = NA, color = "blue", linetype = "dashed") + 
-  geom_sf(data = sf_Crozier, color = "purple", size = 1.7, alpha = 0.5) + 
+  #geom_sf(data = sf_Crozier, color = "purple", size = 1.7, alpha = 0.5) + 
   labs( 
     x = "Easting", 
     y = "Northing", 
@@ -56,6 +57,69 @@ mesh5.5_plot_Crozier <- ggplot() +
   theme_minimal()
 # geom_sf converts to degrees
 
-mesh5.5_plot_Crozier
+mesh_plot_Crozier
+# to bring in boundary, adjust bound.outer
+
+# use Cape Crozier 2019 GA and points
+# for model training as it has better precison and
+# foercasts rather than hindcasts
 
 # create the spatial random field
+# fit an LGCP model to the locations of the penguin nests
+
+# start with prior for spatial range using half the study area
+# [,1] for x range
+0.5*(diff(range(st_coordinates(sf_Crozier)[,1])))
+# 1255.526 metres
+
+# the probability of the range exceeding that to be 0.5
+# the prior for the variance explained by the spatial effect
+# is set that the probability that the SD exceeds 1 is 0.5
+
+# define the SPDE prior (matern)
+matern <- inla.spde2.pcmatern(mesh = Crozier_mesh,
+                            prior.range = c(1000, 0.5), # try 100
+                            prior.sigma = c(1, 0.5))
+
+# specify a model where for 2D models geometry is on the left of ~
+# and an SPDE + Intercept(1) on the right
+# define the domain of the LGCP and model components
+# (spatial SPDE effect and Intercept)
+null_cmp <- geometry ~
+  Intercept(1) + # fixed effect (add covariates by slope of rasters)
+  mySmooth(geometry, model = matern) # random effect
+
+# formula (geometry is response = point locations)
+# mysmooth spatial autocorrelation
+
+# use lgcp() with 2D model components, the sf points and the sf boundary
+null_model <- lgcp(null_cmp, # formula
+                   data = sf_Crozier, # locations
+                   samplers = sf_Crozier_guano, # samplers
+                   domain = list(geometry = Crozier_mesh), # mesh
+                   options = ) # no control fix
+# took several hours to run (could indicate bad model)
+
+summary(null_model)
+
+# predicting intensity
+# predict the spatial intensity surface
+lambda <- predict(
+  null_model,
+  fm_pixels(Crozier_mesh, format = "sf", mask = sf_Crozier_guano),
+  ~ exp(mySmooth + Intercept) # to unlog, mySmooth is field
+)
+
+sum(lambda$mean) # wayy to big
+
+# #pred <- predict(
+#   null_model,
+#   fm_pixels(Crozier_mesh, format = "sf", mask = sf_Crozier_guano),
+#   ~ data.frame(
+#     lamda = exp(mySmooth + Intercept),
+#     loglambda = mySmooth + Intercept
+#   )
+# )
+
+# can plot log and and log lambda intensity
+
