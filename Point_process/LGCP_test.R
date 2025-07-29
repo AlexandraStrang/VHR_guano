4e6ec9bad0bd31ca88415a9a329ca9c7faa68738
Alexandra Strang <alexandra.strang@pg.canterbury.ac.nz>
2025-07-29 15:28:57 +1200

    Update LGCP_test.R

--- Point_process/LGCP_test.R
+++ Point_process/LGCP_test.R
@@ -14,9 +14,13 @@ library(inlabru) # for lgcp()
 
 # EPSG 3031 is in metres but when plotting geom_sf will convert to degrees
 
+# Construct file path
+path <- file.path("home", "user", "documents", "file.txt")
+print(path)
+
 # read in points from xy csv
 # Cape Crozier 2020
-Crozier_xy <- read.csv("Crozier_Points_3031.csv")
+Crozier_xy <- read.csv("Crozier_Points_2020_3031.csv")
 
 # convert to sf object and check CRS
 sf_Crozier <- st_as_sf(Crozier_xy, coords = c("x", "y"), crs = 3031)
@@ -24,32 +28,36 @@ st_crs(sf_Crozier)
 
 # import guano area shapefiles
 sf_Crozier_guano <- st_read("Point_process_GA_boundaries/Crozier_2020_1_3031_guano.shp")
-
 # ensure shapefile has right crs code
 sf_Crozier_guano <- st_transform(sf_Crozier_guano, crs = st_crs(sf_Crozier))
+
+# import Crozier coastline boundary
+sf_Crozier_boundary <- st_read("Crozier_boundary.shp")
+# transform shapefile so it has the right crs code
+sf_Crozier_boundary <- st_transform(sf_Crozier_boundary, crs = st_crs(sf_Crozier))
+st_crs(sf_Crozier_boundary)
 
 # mesh parameters
 # this is 1/15 study size using x range
 # x range is longer than y here
 Crozier_max.edge <- diff(range(st_coordinates(sf_Crozier)[,1]))/(3*5)
-# 160 metres
+# ~150 metres
 
 # expand outer layer out by 1/5
 Crozier_bound.outer = diff(range(st_coordinates(sf_Crozier)[,1]))/5
-# 500 metres
+# ~500 metres
 
 # create finer Crozier mesh
-Crozier_mesh <- fm_mesh_2d(boundary = sf_Crozier_guano, # change to coastline
+Crozier_mesh <- fm_mesh_2d(boundary = sf_Crozier_boundary, # use coastline as boundary
                             max.edge = c(1,5)*Crozier_max.edge, # inner and outer max edge where outer layer has triangle density lower than inner
                             offset = c(Crozier_max.edge, Crozier_bound.outer),
                             cutoff = Crozier_max.edge/10,
                             crs = st_crs(sf_Crozier))
 
-# plot GA boundary mesh with points
+# plot boundary mesh
 mesh_plot_Crozier <- ggplot() + 
   geom_fm(data = Crozier_mesh) + 
-  geom_sf(data = sf_Crozier_guano, fill = NA, color = "blue", linetype = "dashed") + 
-  geom_sf(data = sf_Crozier, color = "purple", size = 1.7, alpha = 0.5) + 
+  geom_sf(data = sf_Crozier_boundary, fill = NA, color = "blue", linetype = "dashed") + 
   labs( 
     x = "Easting", 
     y = "Northing", 
@@ -58,7 +66,44 @@ mesh_plot_Crozier <- ggplot() +
 # geom_sf converts to degrees
 
 mesh_plot_Crozier
-# to bring in boundary, adjust bound.outer
+
+# plot boundary and GA
+mesh_plot_Crozier2 <- ggplot() + 
+  geom_fm(data = Crozier_mesh) + 
+  geom_sf(data = sf_Crozier_boundary, fill = NA, color = "blue", linetype = "dashed") + 
+  geom_sf(data = sf_Crozier_guano, fill = NA, color = "red", linetype = "dashed") + 
+  #geom_sf(data = sf_Crozier, color = "purple", size = 1.7, alpha = 0.5) + 
+  labs( 
+    x = "Easting", 
+    y = "Northing", 
+  ) + 
+  theme_minimal()
+# geom_sf converts to degrees
+
+mesh_plot_Crozier2
+
+# plot boundary, GA, and points
+mesh_plot_Crozier3 <- ggplot() + 
+  geom_fm(data = Crozier_mesh) + 
+  geom_sf(data = sf_Crozier_boundary, fill = NA, color = "blue", linetype = "dashed") + 
+  geom_sf(data = sf_Crozier_guano, fill = NA, color = "red", linetype = "dashed") + 
+  geom_sf(data = sf_Crozier, color = "purple", size = 1.7, alpha = 0.5) + 
+  labs( 
+    x = "Easting", 
+    y = "Northing", 
+  ) + 
+  theme_minimal()
+# geom_sf converts to degrees
+
+mesh_plot_Crozier3
+
+# plot together
+Together <- plot(ggarrange(mesh_plot_Crozier, 
+                           mesh_plot_Crozier2, 
+                           mesh_plot_Crozier3,
+                           ncol = 3, nrow = 1, labels=c("a","b","c")))
+#annotate_figure(Together, left = "Northing", bottom = "Easting")
+
 
 # use Cape Crozier 2019 GA and points
 # for model training as it has better precison and
