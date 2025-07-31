3b8640ae5be91970d6519da0e18cb1290810847a
Alexandra Strang <alexandra.strang@pg.canterbury.ac.nz>
2025-08-01 10:47:16 +1200

    Update LGCP_test.R

--- Point_process/LGCP_test.R
+++ Point_process/LGCP_test.R
@@ -66,108 +66,108 @@ Crozier_mesh <- fm_mesh_2d(boundary = sf_Crozier_boundary, # use coastline as bo
                             crs = st_crs(sf_Crozier))
 
 # plot boundary mesh
-mesh_plot_Crozier <- ggplot() + 
-  geom_fm(data = Crozier_mesh) + 
-  geom_sf(data = sf_Crozier_boundary, fill = NA, color = "blue", linetype = "dashed") + 
-  labs( 
-    x = "Easting", 
-    y = "Northing", 
-  ) + 
-  theme_minimal()
-# geom_sf converts to degrees
+# mesh_plot_Crozier <- ggplot() + 
+#   geom_fm(data = Crozier_mesh) + 
+#   geom_sf(data = sf_Crozier_boundary, fill = NA, color = "blue", linetype = "dashed") + 
+#   labs( 
+#     x = "Easting", 
+#     y = "Northing", 
+#   ) + 
+#   theme_minimal()
+# # geom_sf converts to degrees
 
-mesh_plot_Crozier
+# mesh_plot_Crozier
 
-# plot boundary and GA
-mesh_plot_Crozier2 <- ggplot() + 
-  geom_fm(data = Crozier_mesh) + 
-  geom_sf(data = sf_Crozier_boundary, fill = NA, color = "blue", linetype = "dashed") + 
-  geom_sf(data = sf_Crozier_guano, fill = NA, color = "red", linetype = "dashed") + 
-  #geom_sf(data = sf_Crozier, color = "purple", size = 1.7, alpha = 0.5) + 
-  labs( 
-    x = "Easting", 
-    y = "Northing", 
-  ) + 
-  theme_minimal()
-# geom_sf converts to degrees
+# # plot boundary and GA
+# mesh_plot_Crozier2 <- ggplot() + 
+#   geom_fm(data = Crozier_mesh) + 
+#   geom_sf(data = sf_Crozier_boundary, fill = NA, color = "blue", linetype = "dashed") + 
+#   geom_sf(data = sf_Crozier_guano, fill = NA, color = "red", linetype = "dashed") + 
+#   #geom_sf(data = sf_Crozier, color = "purple", size = 1.7, alpha = 0.5) + 
+#   labs( 
+#     x = "Easting", 
+#     y = "Northing", 
+#   ) + 
+#   theme_minimal()
+# # geom_sf converts to degrees
 
-mesh_plot_Crozier2
+# mesh_plot_Crozier2
 
-# plot boundary, GA, and points
-mesh_plot_Crozier3 <- ggplot() + 
-  geom_fm(data = Crozier_mesh) + 
-  geom_sf(data = sf_Crozier_boundary, fill = NA, color = "blue", linetype = "dashed") + 
-  geom_sf(data = sf_Crozier_guano, fill = NA, color = "red", linetype = "dashed") + 
-  geom_sf(data = sf_Crozier, color = "purple", size = 1.7, alpha = 0.5) + 
-  labs( 
-    x = "Easting", 
-    y = "Northing", 
-  ) + 
-  theme_minimal()
-# geom_sf converts to degrees
+# # plot boundary, GA, and points
+# mesh_plot_Crozier3 <- ggplot() + 
+#   geom_fm(data = Crozier_mesh) + 
+#   geom_sf(data = sf_Crozier_boundary, fill = NA, color = "blue", linetype = "dashed") + 
+#   geom_sf(data = sf_Crozier_guano, fill = NA, color = "red", linetype = "dashed") + 
+#   geom_sf(data = sf_Crozier, color = "purple", size = 1.7, alpha = 0.5) + 
+#   labs( 
+#     x = "Easting", 
+#     y = "Northing", 
+#   ) + 
+#   theme_minimal()
+# # geom_sf converts to degrees
 
-mesh_plot_Crozier3
+# mesh_plot_Crozier3
 
-# plot together
-Together <- plot(ggarrange(mesh_plot_Crozier, 
-                           mesh_plot_Crozier2, 
-                           mesh_plot_Crozier3,
-                           ncol = 3, nrow = 1, labels=c("a","b","c")))
-#annotate_figure(Together, left = "Northing", bottom = "Easting")
+# # plot together
+# Together <- plot(ggarrange(mesh_plot_Crozier, 
+#                            mesh_plot_Crozier2, 
+#                            mesh_plot_Crozier3,
+#                            ncol = 3, nrow = 1, labels=c("a","b","c")))
+# #annotate_figure(Together, left = "Northing", bottom = "Easting")
 
 
-# use Cape Crozier 2019 GA and points
-# for model training as it has better precison and
-# foercasts rather than hindcasts
+# # use Cape Crozier 2019 GA and points
+# # for model training as it has better precison and
+# # foercasts rather than hindcasts
 
-# create the spatial random field
-# fit an LGCP model to the locations of the penguin nests
+# # create the spatial random field
+# # fit an LGCP model to the locations of the penguin nests
 
-# start with prior for spatial range using half the study area
-# [,1] for x range
-0.5*(diff(range(st_coordinates(sf_Crozier)[,1])))
-# 1255.526 metres
+# # start with prior for spatial range using half the study area
+# # [,1] for x range
+# 0.5*(diff(range(st_coordinates(sf_Crozier)[,1])))
+# # 1255.526 metres
 
-# the probability of the range exceeding that to be 0.5
-# the prior for the variance explained by the spatial effect
-# is set that the probability that the SD exceeds 1 is 0.5
+# # the probability of the range exceeding that to be 0.5
+# # the prior for the variance explained by the spatial effect
+# # is set that the probability that the SD exceeds 1 is 0.5
 
-# define the SPDE prior (matern)
-matern <- inla.spde2.pcmatern(mesh = Crozier_mesh,
-                            prior.range = c(1000, 0.5), # try 100 or 10m (might be like 5m?)
-                            prior.sigma = c(1, 0.5))
+# # define the SPDE prior (matern)
+# matern <- inla.spde2.pcmatern(mesh = Crozier_mesh,
+#                             prior.range = c(1000, 0.5), # try 100 or 10m (might be like 5m?)
+#                             prior.sigma = c(1, 0.5))
 
-# specify a model where for 2D models geometry is on the left of ~
-# and an SPDE + Intercept(1) on the right
-# define the domain of the LGCP and model components
-# (spatial SPDE effect and Intercept)
-null_cmp <- geometry ~
-  Intercept(1) + # fixed effect (eventually add here the covariates of slope etc. using rasters)
-  mySmooth(geometry, model = matern) # random effect
+# # specify a model where for 2D models geometry is on the left of ~
+# # and an SPDE + Intercept(1) on the right
+# # define the domain of the LGCP and model components
+# # (spatial SPDE effect and Intercept)
+# null_cmp <- geometry ~
+#   Intercept(1) + # fixed effect (eventually add here the covariates of slope etc. using rasters)
+#   mySmooth(geometry, model = matern) # random effect
 
-# formula (geometry is response = point locations)
-# mysmooth spatial autocorrelation
+# # formula (geometry is response = point locations)
+# # mysmooth spatial autocorrelation
 
-# use lgcp() with 2D model components, the sf points and the sf boundary
-null_model <- lgcp(null_cmp, # formula
-                   data = sf_Crozier, # locations
-                   samplers = sf_Crozier_UAV_area, # sample area of UAV bounds
-                   domain = list(geometry = Crozier_mesh), # mesh
-                   options = ) # no control fix
-# took several hours to run (could indicate bad model)
+# # use lgcp() with 2D model components, the sf points and the sf boundary
+# null_model <- lgcp(null_cmp, # formula
+#                    data = sf_Crozier, # locations
+#                    samplers = sf_Crozier_UAV_area, # sample area of UAV bounds
+#                    domain = list(geometry = Crozier_mesh), # mesh
+#                    options = ) # no control fix
+# # took several hours to run (could indicate bad model)
 
-summary(null_model)
-# mean should be desnity per m 2
+# summary(null_model)
+# # mean should be desnity per m 2
 
-# predicting intensity
-# predict the spatial intensity surface
-lambda <- predict(
-  null_model,
-  fm_pixels(Crozier_mesh, format = "sf", mask = sf_Crozier_guano),
-  ~ exp(mySmooth + Intercept) # to unlog, mySmooth is field
-)
+# # predicting intensity
+# # predict the spatial intensity surface
+# lambda <- predict(
+#   null_model,
+#   fm_pixels(Crozier_mesh, format = "sf", mask = sf_Crozier_guano),
+#   ~ exp(mySmooth + Intercept) # to unlog, mySmooth is field
+# )
 
-sum(lambda$mean) # check how big
+# sum(lambda$mean) # check how big
 
-# can plot log and and log lambda intensity
+# # can plot log and and log lambda intensity
 
