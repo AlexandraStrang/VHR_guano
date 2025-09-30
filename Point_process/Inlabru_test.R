84a6cda844f5e90ac732faf120b5d57bba5404cb
Alexandra Strang <alexandra.strang@pg.canterbury.ac.nz>
2025-09-30 15:59:09 +1300

    Update Inlabru_test.R

--- Point_process/Inlabru_test.R
+++ Point_process/Inlabru_test.R
@@ -61,9 +61,14 @@ Crozier_mesh <- fm_mesh_2d(boundary = buff_boundary,
                            crs = st_crs(sf_Crozier))
 print(Crozier_mesh$n)
 
+# subdivide mesh
+# splits triangles into subtriangles
+mesh_sub <- fm_subdivide(Crozier_mesh,3)
+print(mesh_sub$n)
+
 # plot boundary mesh
 mesh_plot_Crozier <- ggplot() + 
-  geom_fm(data = Crozier_mesh) + 
+  geom_fm(data = mesh_sub) + 
   labs( 
     x = "Easting", 
     y = "Northing", 
@@ -73,6 +78,219 @@ mesh_plot_Crozier <- ggplot() +
 
 mesh_plot_Crozier
 
+# add covariates at 2m res (aggregated below)
+
+# use continuous guano raster
+percent_guano_raster <- rast("CrozierGuano_2m.tif")
+
+# change guano crs to match 
+crs(percent_guano_raster) <- "EPSG:3031"
+crs(percent_guano_raster)
+
+res(percent_guano_raster) # 2m x 2m
+
+# terrain rasters clipped by mesh
+slope_raster <- rast("Crozier_terrain_mesh/Cape_Crozier_slope.tif") # 2m slope raster
+aspect_raster <- rast("Crozier_terrain_mesh/Cape_Crozier_aspect_corrected.tif") # 2m aspect raster
+roughness_raster <- rast("Crozier_terrain_mesh/Cape_Crozier_Roughness.tif") # 2m
+TRI_raster <- rast("Crozier_terrain_mesh/Cape_Crozier_TRI.tif") # 2m
+
+# change corrected aspect crs to match 
+crs(aspect_raster) <- "EPSG:3031"
+crs(aspect_raster)
+
+# match guano area extent to terrain rasters
+percent_guano_raster <- extend(percent_guano_raster, slope_raster)
+
+percent_guano_raster[values(percent_guano_raster) > 1] <- 0
+percent_guano_raster[is.na(percent_guano_raster)] <- 0
+
+# Extract values from raster
+vals <- values(percent_guano_raster)
+
+# Remove NAs and filter out 0s and 1s
+filtered_vals <- vals[!is.na(vals)]
+mean(filtered_vals)
+
+# Plot histogram of intermediate values only
+hist(filtered_vals,
+     main = "Histogram of Guano Percent Values (Excluding 0 & 1)",
+     xlab = "Value",
+     ylab = "Frequency",
+     col = "skyblue",
+     breaks = 100)
+
+plot(percent_guano_raster)
+
+# scale
+# (mean of 0 sd of 1)
+standardize <- function(r) {
+  m <- global(r, fun = "mean", na.rm = TRUE)[1, 1]
+  s <- global(r, fun = "sd", na.rm = TRUE)[1, 1]
+  (r - m) / s
+}
+
+# apply to continuous variables
+percent_guano_raster   <- standardize(percent_guano_raster)
+slope_raster     <- standardize(slope_raster)
+aspect_raster    <- standardize(aspect_raster)
+roughness_raster <- standardize(roughness_raster)
+TRI_raster       <- standardize(TRI_raster)
+
+hist(percent_guano_raster)
+summary(percent_guano_raster)
+
+# have to run this again
+percent_guano_raster[is.na(percent_guano_raster)] <- 0
+
+# check for misalignment
+covariate_plot <- c(percent_guano_raster, slope_raster, aspect_raster, roughness_raster, TRI_raster)
+plot(covariate_plot)
+
+# stack covariates
+cov_stack <- c(percent_guano_raster, slope_raster, aspect_raster, roughness_raster, TRI_raster)
+
 # use inlabru to handle abundance model (positive integer response)
 
-# species 
+# species counts are recorded at each observed location
+# the count model is a coarse aggregation of point process model
+
+# rasterise the species counts to match the spatial covariates
+# mask the regions outside of the study area (buff coastline boundary)
+
+# prepare response variable
+count_raster <- 
+  terra::rasterize(vect(sf_Crozier), cov_stack, fun = sum, background = 0) %>%
+  terra::aggregate(fact = 5, fun = sum) %>%
+  mask(vect(sf::st_geometry(buff_boundary)))
+
+plot(count_raster)
+
+count_raster <- count_raster %>%
+  cellSize(unit = "m") %>%
+  c(count_raster)
+
+res(count_raster)
+summary(count_raster)
+# 149 penguins in 100 m2
+
+# extract the coordinates for these pixels
+counts_df <- crds(count_raster, df = TRUE, na.rm = TRUE) %>%
+  bind_cols(values(count_raster, mat = TRUE, na.rm = TRUE)) %>%
+  rename(count = sum) %>%
+  mutate(present = (count > 0) *1L) %>%
+  st_as_sf(coords = c("x", "y"), crs = st_crs(sf_Crozier))
+
+# aggregate covariates to match count raster
+cov_stack <- terra::aggregate(cov_stack, fact = 5, fun = mean)
+plot(cov_stack)
+
+# SPDE priors
+matern <- inla.spde2.pcmatern(mesh = mesh_sub,
+                              prior.range = c(100, 0.9), 
+                              prior.sigma = c(0.01, 0.1))
+
+# plot of non-zero counts
+ggplot() +
+  geom_fm(data = mesh_sub) +
+  geom_sf(
+    data = counts_df[counts_df$count > 0, ],
+    aes(color = count),
+    size = 1,
+    pch = 4
+  ) +
+  theme_minimal()
+
+# Poisson GLM
+# Poisson model that links species counts per raster cell to spatial covariates
+
+cmps <- ~ percentguano(cov_stack$CrozierGuano_2m, model = "linear") +
+  slope(cov_stack$Cape_Crozier_slope, model = "linear") +
+  field(geometry, model = matern) + Intercept(1)
+
+fit_poi <- bru(
+  cmps,
+  bru_obs(
+    family = "poisson", data = counts_df,
+    formula = count ~
+      percentguano + slope +
+      field + Intercept,
+    E = area
+  )
+)
+
+summary(fit_poi)
+
+pred_poi <- predict(
+  fit_poi, counts_df,
+  ~{
+    expect <- exp(percentguano +
+                    slope +
+                    field + Intercept
+                  ) * area
+    list(
+      expect = expect,
+      obs_prob = dpois(count, expect)
+    )
+  },
+  n.samples = 1000
+)
+
+expect_poi <- pred_poi$expect
+expect_poi$pred_var <- expect_poi$mean + expect_poi$sd^2
+expect_poi$log_score <- -log(pred_poi$obs_prob$mean)
+
+predicted_abundance <- sum(expect_poi$mean)
+
+ggplot() +
+  geom_fm(data = mesh_sub) +
+  gg(expect_poi, aes(fill = mean / area), geom = "tile") +
+  #geom_sf(data = sf_Crozier, color = "red", size = 1, pch = 4, alpha = 0.2) +
+  ggtitle("Nest intensity per ~ m")
+
+# add covariates to cell raster
+
+# Extract covariate values from aggregated cov_stack into counts_df
+cov_values <- terra::extract(cov_stack, vect(counts_df))[, -1]
+counts_df <- bind_cols(counts_df, cov_values)
+
+# compute cell area from covariate raster
+area_raster <- terra::cellSize(cov_stack[[1]], unit = "m")
+
+# extract area values to prediction grid
+area_vals <- terra::extract(area_raster, vect(pred_grid))[,2]
+pred_grid$area <- area_vals
+
+pred_map <- predict(
+  fit_poi, pred_grid,
+  ~{
+    expect <- exp(percentguano +
+                    slope +
+                    field + Intercept
+    ) * area
+    list(
+      expect = expect
+    )
+  },
+  n.samples = 1000
+)
+
+pred_grid$expected_abundance <- pred_map$expect$mean
+pred_raster <- terra::rasterize(vect(pred_grid), cov_stack, field = "expected_abundance")
+plot(pred_raster)
+
+total_abundance <- sum(pred_map$expect$mean, na.rm = TRUE)
+
+
+pred_grid$linear_predictor <- pred_map$percentguano +
+  pred_map$slope +
+  pred_map$field +
+  pred_map$Intercept
+
+hist(pred_grid$linear_predictor)
+
+
+
+# ZIP model?
+# Zero-Inflated Poisson model
+
