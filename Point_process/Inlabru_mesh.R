55dea09512844f3437652c080cf15ac39dbeb68e
Alexandra Strang <alexandra.strang@pg.canterbury.ac.nz>
2026-03-19 11:46:13 +1300

    Update Inlabru_mesh.R

--- Point_process/Inlabru_mesh.R
+++ Point_process/Inlabru_mesh.R
@@ -3,7 +3,7 @@
 # created: 2025
 
 # set working directory
-setwd("C:/Users/astra/OneDrive - University of Canterbury/ANTA - PhD/Data/Inlabru/Inlabru_data")
+setwd("C:/Users/ajs424/OneDrive - University of Canterbury/ANTA - PhD/Data/Inlabru/Inlabru_data")
 
 # load packages 
 library(sf)
@@ -16,13 +16,21 @@ library(INLA)
 
 # EPSG 3031 is in metres but when plotting geom_sf will convert to degrees
 
+##############################################################################################
+# Load data
+##############################################################################################
+
 # read in points from xy csv
 # Cape Crozier 2020
-Crozier_xy <- read.csv("Crozier_Points_2020_3031.csv")
+Crozier_xy <- read.csv("Crozier_UAV_points/Reprojected_3031/Crozier_2020_Points_3031.csv")
 
 # convert to sf object and check CRS
 sf_Crozier <- st_as_sf(Crozier_xy, coords = c("x", "y"), crs = 3031)
 st_crs(sf_Crozier)
+
+##############################################################################################
+# Mesh
+##############################################################################################
 
 # import Crozier coastline boundary
 sf_Crozier_boundary <- st_read("Crozier_boundary.shp")
@@ -50,8 +58,7 @@ Crozier_mesh <- fm_mesh_2d(boundary = buff_boundary,
                            cutoff = 0.3,
                            crs = st_crs(sf_Crozier))
 print(Crozier_mesh$n)
-
-# mesh gets subdivided by 3 (max edge 30m)
+# 2816
 
 # plot boundary mesh
 mesh_plot_Crozier <- ggplot() + 
@@ -65,9 +72,39 @@ mesh_plot_Crozier <- ggplot() +
 
 mesh_plot_Crozier
 
-# save mesh as shapefile - Koerich et al.
+# subdivide mesh
+# splits triangles into subtriangles
+# mesh gets subdivided by 3 (max edge 30m)
+mesh_sub <- fm_subdivide(Crozier_mesh,3)
+print(mesh_sub$n)
+# 44519
+
+# plot mesh with coastline boundary
+mesh_plot_Crozier <- ggplot() + 
+  geom_fm(data = mesh_sub) + 
+  geom_sf(data = buff_boundary, fill = NA, color = "blue") +
+  geom_sf(data = sf_Crozier, color = "purple", size = 1.7, alpha = 0.5) + 
+  labs( 
+    x = "Easting", 
+    y = "Northing", 
+  ) + 
+  theme_minimal()
+# geom_sf converts to degrees
+
+mesh_plot_Crozier
+ggsave("Inlabru_outputs/Mesh_plot.png", mesh_plot_Crozier,
+       width = 8, height = 5, units = "in",
+       dpi = 600
+)
+
+# save outputs
+saveRDS(mesh_sub,      file = "Inlabru_outputs/mesh_sub.rds")
+saveRDS(buff_boundary, file = "Inlabru_outputs/buff_boundary.rds")
+
+# save unsubdivded mesh as shapefile - Koerich et al.
 # to crop terrain variables to mesh boundary
 # save Cozier mesh (do once)
+
 vertices <- Crozier_mesh$loc  # Vertex coordinates
 triangles <- Crozier_mesh$graph$tv  # Triangle indices
 
